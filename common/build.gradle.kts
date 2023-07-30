d778e39e1ce4ae989b1d82d6aed27ebcd92ce451
mcarr <136939846+mcarr823@users.noreply.github.com>
2023-07-30 20:59:02 +1000

    Removed unused dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.compose.compose
 
 val coroutinesVersion = "1.7.1"
 val ktorVersion = "2.3.1"
-val sqlDelightVersion = "1.5.5"
 val dateTimeVersion = "0.4.0"
 val nav_version = "2.0.0"
 
@@ -12,7 +11,6 @@ plugins {
     id("org.jetbrains.compose")
     id("com.android.library")
     kotlin("plugin.serialization") version "1.8.22"
-    id("com.squareup.sqldelight")
     id("kotlin-parcelize")
 
 }
@@ -36,7 +34,6 @@ kotlin {
                 implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
                 implementation("io.ktor:ktor-client-cio:$ktorVersion")
                 implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
-                implementation("com.squareup.sqldelight:runtime:$sqlDelightVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:$dateTimeVersion")
                 implementation("com.arkivanov.decompose:decompose:$nav_version")
                 implementation("com.arkivanov.decompose:extensions-compose-jetbrains:$nav_version")
@@ -56,7 +53,6 @@ kotlin {
             dependencies {
                 api("androidx.appcompat:appcompat:1.5.1")
                 api("androidx.core:core-ktx:1.9.0")
-                implementation("com.squareup.sqldelight:android-driver:$sqlDelightVersion")
                 implementation("com.arkivanov.decompose:decompose:$nav_version")
                 implementation("com.arkivanov.decompose:extensions-compose-jetpack:$nav_version")
             }

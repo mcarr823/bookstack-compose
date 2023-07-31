b1ba5ce9a4420e05d3e6f0459184f0368291a949
mcarr <136939846+mcarr823@users.noreply.github.com>
2023-07-31 20:18:00 +1000

    Setup screen now performs a connectivity check when saving. Button is also disabled and progress indicator shown while performing check.

--- common/src/commonMain/kotlin/dev/mcarr/common/ui/screens/Setup.kt
+++ common/src/commonMain/kotlin/dev/mcarr/common/ui/screens/Setup.kt
@@ -1,18 +1,31 @@
 package dev.mcarr.common.ui.screens
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material.Button
+import androidx.compose.material.Checkbox
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
+import androidx.compose.material.ProgressIndicatorDefaults
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import dev.mcarr.common.network.API
+import kotlinx.coroutines.launch
 
 @Composable
 fun Setup(
@@ -22,6 +35,7 @@ fun Setup(
     connect: (endpoint: String, tokenId: String, tokenSecret: String) -> Unit
 ) {
 
+    val coroutineScope = rememberCoroutineScope()
     var endpoint by rememberSaveable(stateSaver = TextFieldValue.Saver) {
         mutableStateOf(TextFieldValue(defaultEndpoint))
     }
@@ -31,6 +45,9 @@ fun Setup(
     var tokenSecret by rememberSaveable(stateSaver = TextFieldValue.Saver) {
         mutableStateOf(TextFieldValue(defaultTokenSecret))
     }
+    var saving by remember { mutableStateOf(false) }
+    var message by remember { mutableStateOf("") }
+    var messageColor by remember { mutableStateOf(Color.Red) }
 
 
     Column(
@@ -45,7 +62,7 @@ fun Setup(
         )
         Text(
             text = "Base URL or domain. eg. https://my.domain.com or my.domain.com",
-            modifier = Modifier.padding(16.dp, 0.dp,0.dp,0.dp)
+            modifier = Modifier.padding(16.dp, 0.dp, 0.dp, 0.dp)
         )
         Spacer(
             modifier = Modifier.height(20.dp)
@@ -65,12 +82,42 @@ fun Setup(
         )
         Button(
             onClick = {
-                connect(endpoint.text, tokenId.text, tokenSecret.text)
+                if (!saving) {
+                    saving = true
+                    message = ""
+                    coroutineScope.launch {
+                        val api = API(
+                            apiUrl = endpoint.text,
+                            tokenId = tokenId.text,
+                            tokenSecret = tokenSecret.text,
+                            disableHttpsVerification = true,
+                            testing = true
+                        )
+                        try {
+                            api.getDocsHtml()
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                            messageColor = Color.Red
+                            message = e.localizedMessage
+                        }
+                        saving = false
+                        if (message.isEmpty()) {
+                            messageColor = Color.Green
+                            message = "Connection successful"
+                            connect(api)
+                        }
+                    }
+                }
             },
             modifier = Modifier.padding(16.dp).align(Alignment.End)
-        ){
+        ) {
+            if (saving) {
+                CircularProgressIndicator(
+                    color = Color.Red
+                )
+            }
             Text(
-                text = "Connect",
+                text = "Save",
                 modifier = Modifier.padding(8.dp)
             )
         }
